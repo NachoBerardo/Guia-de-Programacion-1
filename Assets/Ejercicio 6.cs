e5c9bab3021c49e042cbfbfcc8f4dbe4f07e8bc0
NachoBerardo <101335543+NachoBerardo@users.noreply.github.com>
2022-03-28 12:11:52 -0300

    Ejercicio 6 sin completar

---
+++ Assets/Ejercicio 6.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6 : MonoBehaviour
+{
+    //6. realizá un programa que permita el ingreso por inspector de un valor entero mayor que 0 en una variable llamada num1 y muestre un mensaje
+    //por pantalla indicando "el  número es par" o "el número es impar". deberá utilizar el operador “módulo” es el caracter  %.
+
+    public int num1;
+
+    void Start()
+    {
+        if (num1 > 0)
+        {
+            if (num1 % num1 == 0)
+            {
+                
+            }
+        }
+
+        else
+        {
+            Debug.Log("Ingrese un numero mayor a 0");
+        }
+
+    }
+
+
+}
