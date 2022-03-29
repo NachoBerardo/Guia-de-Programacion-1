46598952ce2ee64ba12998e5cb431c4eb688c212
NachoBerardo <101335543+NachoBerardo@users.noreply.github.com>
2022-03-29 13:34:51 -0300

    Ejercicio 8 y 9
    Competos

---
+++ Assets/Ejercicio_8.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_8 : MonoBehaviour
+{
+    //8. Crear una variable entera que pueda ser modificada desde el Inspector.Suponiendo que el primer día de la semana es el Domingo,
+    //mostrar el nombre del día correspondiente al valor de la variable “dia”. Si el día no está en el rango  permitido debe emitir
+    //por pantalla el mensaje de error "El día ingresado no es válido". 
+
+    public int dia;
+
+    void Start()
+    {
+        if (dia == 1)
+        {
+            Debug.Log("Domingo");
+        }
+
+        else if (dia == 2)
+        {
+            Debug.Log("Lunes");
+        }
+
+        else if (dia == 3)
+        {
+            Debug.Log("Martes");
+        }
+
+        else if (dia == 4)
+        {
+            Debug.Log("Miercoles");
+        }
+
+        else if (dia == 5)
+        {
+            Debug.Log("Jueves");
+        }
+
+        else if (dia == 6)
+        {
+            Debug.Log("Viernes");
+        }
+
+        else if (dia == 7)
+        {
+            Debug.Log("Sabado");
+        }
+
+        else
+        {
+            Debug.Log("El día ingresado no es válido");
+        }
+
+        
+
+
+    }
+
+
+}
---
+++ Assets/Ejercicio_9.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_9 : MonoBehaviour
+{
+    // 9. Realizá un programa que resuelva el siguiente problema:  
+    //Tres personas aportan diferente capital a una sociedad.Se desea saber qué porcentaje del total aportó cada una (indicando nombre y porcentaje) 
+    //y cuál es el monto del total aportado  por las tres.
+    //Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego  mostrar lo pedido en el siguiente formato:  
+    //Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ …. 
+
+    public int persona1;
+    public int persona2;
+    public int persona3;
+
+    void Start()
+    {
+        int total = persona1 + persona2 + persona3;
+        Debug.Log("Nombre: Persona1. Capital aportado: " + persona1 + "$. Porcentaje del capital: " + ((persona1 * 100) / total) + "%. Monto total aportado: " + total);
+        Debug.Log("Nombre: Persona2. Capital aportado: " + persona2 + "$. Porcentaje del capital: " + ((persona2 * 100) / total) + "%. Monto total aportado: " + total);
+        Debug.Log("Nombre: Persona3. Capital aportado: " + persona3 + "$. Porcentaje del capital: " + ((persona3 * 100) / total) + "%. Monto total aportado: " + total);
+    }
+
+}
